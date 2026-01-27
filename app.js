991d2d53d63ef777a77325872faf40118ea37732
Chinaglia <156460261+chinaglia77@users.noreply.github.com>
2026-01-27 15:10:21 -0300

    feat: adiciona conexão com banco de dados

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const conexao = require('./config/bancoDeDados')
+
 const { engine } = require('express-handlebars')
 
 const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.engine('handlebars', engine({
   partialsDir: './views/partials'
@@ -16,6 +21,20 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.render('home')
 })
+
+app.post('/registrarEntrada', (req, res) => {
+
+  const { tipo_veiculo, vagas_disponivel, placa, modelo, cor } = req.body;
+
+  console.log(tipo_veiculo);
+  console.log(vagas_disponivel);
+  console.log(placa);
+  console.log(modelo);
+  console.log(cor);
+
+  res.send('Entrada registrada');
+});
+
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000 🚀')
