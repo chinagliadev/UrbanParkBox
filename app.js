9658e02f6dfa1380b497c3050a193babf05b7741
chinagliadev <156460261+chinagliadev@users.noreply.github.com>
2026-02-01 14:40:27 -0300

    feat/consulta dados da tabela vagaS

--- app.js
+++ app.js
@@ -18,30 +18,75 @@ app.set('views', './views')
 app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'))
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
 
-app.get('/:situacao', (req, req)=>{
+app.get('/', async (req, res) => {
+  try {
+    const SQL = "SELECT * FROM vagas WHERE status = 'disponivel'";
+    const [vagas] = await conexao.query(SQL);
 
-})
+    res.render('home', {
+      vagas,
+      erro: null
+    });
 
-app.post('/registrarEntrada', (req, res) => {
+  } catch (err) {
+    console.error(err);
 
-  const { tipo_veiculo, vagas_disponivel, placa, modelo, cor } = req.body;
-
-  if(tipo_veiculo === '' || placa === '' || modelo === '' || cor === ''){
-    res.redirect('/erro')    
+    res.render('home', {
+      vagas: [],
+      erro: 'Erro ao carregar as vagas. Tente novamente mais tarde.'
+    });
   }
-
-  const SQL = "INSERT INTO "
-
-  // conexao.query()
-
-  res.send('Entrada registrada');
 });
 
+app.post('/', async (req, res) => {
+  const { tipo_veiculo, vagas_disponivel, placa, modelo, cor } = req.body;
+  const status = 'ativo';
+
+  if (!tipo_veiculo || !placa || !modelo || !cor) {
+    return res.redirect('/erro');
+  }
+
+  try {
+
+    let vagaId = vagas_disponivel
+    if (vagaId === '') {
+      const SQLVAGAS = "SELECT * FROM vagas WHERE status = 'disponivel' LIMIT 1";
+      const [vagas] = await conexao.query(SQLVAGAS);
+      const vagaDisponivel = vagas[0];
+      const UPDATEVAGA = "UPDATE vagas SET status = 'ocupado' WHERE id = ?";
+      await conexao.query(UPDATEVAGA, [vagaDisponivel.id]);
+
+      return  res.redirect('/');
+    }
+
+    const SQLVEICULO = `
+        INSERT INTO veiculos (tipo, placa, modelo, cor, status)
+        VALUES (?, ?, ?, ?, ?)
+      `;
+
+    const SQLVAGAS = `
+      UPDATE vagas SET status = 'ocupado' WHERE id = ?
+    `
+
+    await conexao.query(SQLVEICULO, [
+      tipo_veiculo,
+      placa,
+      modelo,
+      cor,
+      status,
+    ]);
+
+    await conexao.query(SQLVAGAS, [vagas_disponivel])
+
+    res.redirect('/');
+
+  } catch (err) {
+    console.error(err);
+    res.redirect('/erro');
+  }
+});
 
 app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000 🚀')
+  console.log('Servidor rodando na porta 3000')
 })
